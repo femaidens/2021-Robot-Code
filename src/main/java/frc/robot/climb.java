cad248ee95049ed00069baf93e97c650af03ea29
Kathryn <kathrynlle9@gmail.com>
2021-02-16 17:15:37 -0500

    added climb

--- src/main/java/frc/robot/climb.java
+++ src/main/java/frc/robot/climb.java
@@ -7,7 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /** Add your docs here. */
-public class climb extends Subsystem {
+public class Climb extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
