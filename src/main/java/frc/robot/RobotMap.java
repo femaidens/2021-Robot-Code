d0e9fef90374f1ffaa2ed93e2512fcfe1e848070
Jillian Chong <chongj@bxscience.edu>
2021-02-16 17:42:41 -0500

    shooter things

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class RobotMap {
+	public static final int shooterNEOPort = 2;
+	public static final int hopperExit1Port = 3;
+	public static final int hopperExit2Port = 4;
+
+}
--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -4,12 +4,60 @@
 
 package frc.robot;
 
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /** Add your docs here. */
 public class Shooter extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
+  public CANSparkMax shooterNEO = new CANSparkMax(RobotMap.shooterNEOPort, MotorType.kBrushless);
+	public CANSparkMax hopperExit1 = new CANSparkMax(RobotMap.hopperExit1Port, MotorType.kBrushless);
+	public CANSparkMax hopperExit2 = new CANSparkMax(RobotMap.hopperExit2Port, MotorType.kBrushless);
+
+	public CANPIDController pidController = shooterNEO.getPIDController();
+
+	private final double yDisplacement;
+	//private final double xDisplacement;
+  private final double angle;
+  
+  public Shooter(){
+		pidController.setP(0.0);
+		pidController.setI(0.0);
+		pidController.setD(0.0);
+    pidController.setOutputRange(0.1, 1.0); 
+    pidController.setFF(0.0);
+    pidController.setIZone(0.0);
+  }
+
+  public void spinWheel() {	
+    hopperExit1.set(0.8);	
+    hopperExit2.set(0.8);
+    double speed = calculateSpeed();
+    pidController.setReference(speed, ControlType.kVelocity);
+  }
+    
+  public void stop() {
+    shooterNEO.set(0.0);
+    hopperExit1.set(0.0);
+    hopperExit2.set(0.0);
+  }
+    
+  public double calculateSpeed(){ 
+    double y_speed = Math.sqrt(19.6 * yDisplacement);
+    double speed = y_speed/Math.sin(angle);
+
+    //speed /= radius;
+    return speed;
+  }
+    
 
   @Override
   public void initDefaultCommand() {
