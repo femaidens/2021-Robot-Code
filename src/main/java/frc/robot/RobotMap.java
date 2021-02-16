f1bce58b6ff875400990ba6f8232555965437095
Kathryn <kathrynlle9@gmail.com>
2021-02-16 17:41:51 -0500

    added climb

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class RobotMap {
+    //joystick
+	private final int drivePort = 0;
+	private final int operPort = 1; 
+
+//shooter
+	private final int shooterNEOPort = 2;
+	private final int hopperExit1Port = 3;
+	private final int hopperExit2Port = 4;
+	
+
+	//climb
+	private final int motor775Port = 5;
+	private final int rightNEOPort = 6;
+	private final int leftNEOPort = 7;
+	private final int leftPistonPort1 = 8;
+	private final int rightPistonPort1 = 9;
+	private final int leftPistonPort2 = 10;
+    private final int rightPistonPort2 = 11;
+	
+	//hopper
+    private final int hopperNEOPort = 12;
+    private final int inPort1 = 13;
+    private final int inPort2 = 14;
+    private final int outPort1 = 15;
+    private final int outPort2 = 16;
+
+    //drivetrain
+    private final int frontRightPort = 17;
+    private final int frontLeftPort = 18;
+    private final int middleRightPort = 19;
+    private final int middleLeftPort	= 20;
+    private final int rearRightPort = 21;
+    private final int rearLeftPort = 22;
+
+    //intake
+    private final int intakePistonPort1 = 23;
+    private final int intakePistonPort2 =24;
+    private final int intakeNEOPort = 25;
+
+    //colorwheel
+	private final int colorMotorPort = 26;
+    private final int colorPistonPort1 = 27;
+    private final int colorPistonPort2 = 28; 
+    private final int colorSensorPort = 29;
+
+
+}
