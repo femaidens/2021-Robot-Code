323a49f563904402c1b6e4ad4668feeae238a140
Your Majesty <pajol@bxscience.edu>
2021-02-19 17:40:03 -0500

    added intake subsystems and commands

--- src/main/java/frc/robot/Commands/Stage2.java
+++ src/main/java/frc/robot/Commands/Stage2.java
@@ -2,8 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
-
+package frc.robot.Commands;
+import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Stage2 extends Command {
--- src/main/java/frc/robot/Subsystems/Climb.java
+++ src/main/java/frc/robot/Subsystems/Climb.java
@@ -2,7 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.Subsystems;
+import frc.robot.Robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
